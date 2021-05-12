7b1e11b151ff9d592d7b9dbea54f29f7672a954c
Sommer Colvin <19175475+sommercolvin@users.noreply.github.com>
2021-05-12 15:42:26 -0500

    Implemented sign-up & implemented firebase utils & auth

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -4,9 +4,13 @@ import './sign-in-and-sign-up.styles.scss';
 
 import SignIn from "../../components/sign-in/sign-in.component";
 
+import SignUp from "../../components/sign-up/sign-up.component";
+
+
 const SignInAndSignUpComponent = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn/>
+        <SignUp/>
     </div>
 )
 
